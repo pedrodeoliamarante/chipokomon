a2742c325fc78a07f3fcc2233924bf26d72b8b87
Pedro Amarante <ppedrolia@gmail.com>
2021-02-18 22:59:24 -0300

    added to do txt fixed classes issues added trainer and pokemon library

--- classes.py
+++ classes.py
@@ -1,6 +1,5 @@
 class Chinpokomon:
     def __init__(self, name, level, attack_power, chinpokomon_type, current_health, is_knocked_out):
-        self.is_knocked = True
         self.name = name
         self.level = level
         self.attack_power = attack_power
@@ -20,19 +19,20 @@ class Chinpokomon:
 
     def attack(self, opponent_chinpokomon):
         damage = self.attack_power
-        if self.chinpokomon_type == "grass" and opponent_chinpokomon.type == "water":
+        if self.chinpokomon_type == "grass" and opponent_chinpokomon.chinpokomon_type == "water":
             damage = damage * 2
-        elif self.chinpokomon_type == "fire" and opponent_chinpokomon.type == "grass":
+        elif self.chinpokomon_type == "fire" and opponent_chinpokomon.chinpokomon_type == "grass":
             damage = damage * 2
-        elif self.chinpokomon_type == "water" and opponent_chinpokomon.type == "fire":
+        elif self.chinpokomon_type == "water" and opponent_chinpokomon.chinpokomon_type == "fire":
             damage = damage * 2
         opponent_chinpokomon.lose_health(damage)
-        print("{opponent} was attacked and lost {damage} health points!".format(opponent=opponent_chinpokomon,
+        print("{opponent} was attacked and lost {damage} health points!".format(opponent=opponent_chinpokomon.name,
                                                                                 damage=damage))
 
     def knock_out(self):
         if self.current_health == 0:
             print("{name} was knocked out!".format(name=self.name))
+        self.is_knocked_out = True
 
     def gain_health(self, heal):
         new_health = self.current_health + heal
@@ -51,13 +51,12 @@ class Trainer:
         self.n_of_potions = n_of_potions
         self.active_chinpokomon = active_chinpokomon
 
-    def use_potion(self, active_chinpokomon):
-        self.active_chinpokomon = active_chinpokomon
-        if self.n_of_potions < 0:
+    def use_potion(self):
+        if self.n_of_potions <= 0:
             print("There are no potions left to use!")
         else:
-            active_chinpokomon.gain_health(10)
-            print("{active_chinpokomon} was healed using a potion!".format(active_chinpokomon=self.active_chinpokomon))
+            self.active_chinpokomon.gain_health(10)
+            print("{active_chinpokomon} was healed using a potion!".format(active_chinpokomon=self.active_chinpokomon.name))
             new_number_of_potions = self.n_of_potions - 1
             self.n_of_potions = new_number_of_potions
 
---
+++ pokemon and trainer library.py
@@ -0,0 +1,10 @@
+from classes import Chinpokomon, Trainer
+
+velocirapstar = Chinpokomon("Velocirapstar", 10, 10, "fire", 0, False)
+brocoli = Chinpokomon("Brocoli", 10, 8, "grass", 10, False)
+fetuswami = Chinpokomon("Fetuswami", 10, 9, "water", 10, False)
+donkeytrom = Chinpokomon("Donkeytrom", 11, 7, "water", 10, False)
+
+cartman = Trainer("Cartman", [velocirapstar, fetuswami], 2, velocirapstar)
+kenny = Trainer("Kenny", [brocoli, donkeytrom], 0, donkeytrom)
+
